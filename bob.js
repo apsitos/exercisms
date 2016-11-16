9ff02df078971e590173276f219ae273fd9de7d7
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-11-15 17:07:52 -0700

    stuck on numbers

---
+++ bob.js
@@ -0,0 +1,30 @@
+var Bob = function() {};
+
+// order of if/elses
+
+
+Bob.prototype.hey = function(statement) {
+  onlyInteger();
+  if (statement === statement.toUpperCase()) {
+    return 'Whoa, chill out!'
+  } else if (statement.includes('?')) {
+    return 'Sure.';
+  } else {
+    return 'Whatever.';
+  }
+
+function onlyInteger() {
+  for (i = 0; i < statement.length; i++) {
+    
+  }
+
+}
+
+module.exports = Bob;
+
+
+
+// return statement.includes(integer)
+// return true if it includes an integer
+// split in array
+// check if array includes
