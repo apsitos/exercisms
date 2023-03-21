95f6c9be12b605a45841619150b68134ab7f9a7f
Annastasia Psitos <annastasia.psitos@gmail.com>
2023-03-21 11:03:14 -0600

    3-21 fruit picker

---
+++ Basics-2023/fruit-picker/fruit-picker.js
@@ -0,0 +1,35 @@
+import { order } from "./grocer";
+import { notify } from "./notifier";
+
+/**
+ * @return void
+ */
+export function onSuccess() {
+  return notify({ message: "SUCCESS" });
+}
+
+/**
+ * @return void
+ */
+export function onError() {
+  return notify({ message: "ERROR" });
+}
+
+/**
+ * @param {GrocerQuery} query
+ * @param {FruitPickerSuccessCallback} onSuccessCallback
+ * @param {FruitPickerErrorCallback} onErrorCallback
+ * @return void
+ */
+export function orderFromGrocer(query, onSuccessCallback, onErrorCallback) {
+  return order(query, onSuccessCallback, onErrorCallback);
+}
+
+/**
+ * @param {string} variety
+ * @param {number} quantity
+ * @return void
+ */
+export function postOrder(variety, quantity) {
+  return orderFromGrocer({ variety, quantity }, onSuccess, onError);
+}
